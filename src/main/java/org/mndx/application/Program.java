326bd56f520a4e1b2274b3364a4af560a273826b
Felipe Mendes <lfelipecmendes@icloud.com>
2025-02-05 00:30:22 -0300

    feat: JPA/Hibernate query example

--- src/main/java/org/mndx/application/Program.java
+++ src/main/java/org/mndx/application/Program.java
@@ -8,17 +8,14 @@ import javax.persistence.Persistence;
 
 public class Program {
 		public static void main(String[] args) {
-				Person personOne = new Person(null, "Noah", "noah@example.com");
-				Person personTwo = new Person(null, "Felipe", "felipe@example.com");
-				Person personThree = new Person(null, "Maria", "maria@example.com");
-
 				EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-example");
 				EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-				entityManager.getTransaction().begin();
-				entityManager.persist(personOne);
-				entityManager.persist(personTwo);
-				entityManager.persist(personThree);
-				entityManager.getTransaction().commit();
+				Person person = entityManager.find(Person.class, 1);
+
+				System.out.println(person);
+
+				entityManagerFactory.close();
+				entityManager.close();
 		}
 }
